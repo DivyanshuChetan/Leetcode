5fb8e1dc9b9017ffc45a5a4a70502cba8991f451
Divyanshu Chetan <126164731+DivyanshuChetan@users.noreply.github.com>
2023-08-06 17:18:59 +0530

    Time: 19 ms (54.14%), Space: 13.2 MB (85.39%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.py
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -1,16 +1,16 @@
-# class Solution(object):
-#     def numJewelsInStones(self, jewels, stones):
-#         count=0
-#         for i in stones:
-#             for j in jewels:
-#                 if i==j:
-#                     count+=1
-#         return count
-
 class Solution(object):
     def numJewelsInStones(self, jewels, stones):
         count=0
         for i in stones:
-            if i in jewels:
-                count+=1
+            for j in jewels:
+                if i==j:
+                    count+=1
         return count
+
+# class Solution(object):
+#     def numJewelsInStones(self, jewels, stones):
+#         count=0
+#         for i in stones:
+#             if i in jewels:
+#                 count+=1
+#         return count
