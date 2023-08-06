052bbc052b40ff824d47787519dce43785998a71
Divyanshu Chetan <126164731+DivyanshuChetan@users.noreply.github.com>
2023-08-06 13:24:35 +0530

    Time: 42 ms (99.78%), Space: 13.6 MB (36.96%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.py
+++ 2058-concatenation-of-array/2058-concatenation-of-array.py
@@ -1,11 +1,20 @@
+# class Solution(object):
+#     def getConcatenation(self, nums):
+#         ans=[]
+#         temp=[]
+#         for i in nums:
+#             ans.append(i)
+#         for i in ans:
+#             nums.append(i)
+#         # for i in temp:
+#         #     ans.append(i)
+#         return nums
 class Solution(object):
     def getConcatenation(self, nums):
-        ans=[]
-        temp=[]
-        for i in nums:
-            ans.append(i)
-        for i in ans:
-            nums.append(i)
-        # for i in temp:
-        #     ans.append(i)
-        return nums
+        answer = [0] * (2 * len(nums))
+        j = 0       
+        for i in range(len(nums)):         
+            answer[i] = nums[i]           
+            answer[i + len(nums)] = nums[i]
+        return answer
+
