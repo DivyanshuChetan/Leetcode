db1ec3694253274ae083a0a45dd1dfe6a499c970
Divyanshu Chetan <126164731+DivyanshuChetan@users.noreply.github.com>
2023-08-06 11:49:40 +0530

    Time: 17 ms (59.88%), Space: 13.2 MB (88.96%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
@@ -1,3 +1,13 @@
-class Solution:
-    def interpret(self, command: str) -> str:
-        return command.replace("()","o").replace("(al)","al")
+class Solution(object):
+    def interpret(self, command):
+        res=""
+        
+        for i in range(len(command)):
+            if command[i]=="G":
+                res+="G"
+            elif command[i]=="(" and command[i+1]==")":
+                res+="o"
+            elif command[i]=="(" and command[i+1]=="a":
+                res+="al"
+        return res
+
