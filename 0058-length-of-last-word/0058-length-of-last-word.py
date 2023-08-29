6634d5944df1cdc5b2466fae9ef447b50df496f0
Divyanshu Chetan <126164731+DivyanshuChetan@users.noreply.github.com>
2023-08-30 01:17:24 +0530

    Time: 15 ms (59.27%), Space: 13.5 MB (69.89%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,16 +1,14 @@
 class Solution(object):
     def lengthOfLastWord(self, s):
-        count=0
         space=False
-
-        for i in xrange(len(s)):
-            if s[i]==" ":
-                space=True
-            elif s[i]!=" " and space:
-                count=1
-                space = False
-            else:
-                count +=1
-        return count
-
+        count=0
         
+        for i in s:
+            if i==" ":
+                space=True
+            elif i!=" " and space==True:
+                count=1
+                space=False
+            else:
+                count+=1
+        return count
