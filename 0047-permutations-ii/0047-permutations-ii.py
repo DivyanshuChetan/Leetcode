867d12125162a1eec8a1a9aa4decad0d20c2d91b
Divyanshu Chetan <126164731+DivyanshuChetan@users.noreply.github.com>
2023-10-14 00:01:14 +0530

    Time: 620 ms (10.63%), Space: 16.5 MB (87.59%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,4 +1,14 @@
-from itertools import permutations
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        return set(list(permutations(nums)))
+        ans = []
+        nums.sort()
+        def subset(p, up):
+            if len(up) == 0:
+                if p not in ans:
+                    ans.append(p)
+                return 
+            ch = up[0]
+            for i in range(len(p) + 1):
+                subset(p[0:i] + [ch] + p[i:], up[1:])
+        subset([], nums)
+        return ans
