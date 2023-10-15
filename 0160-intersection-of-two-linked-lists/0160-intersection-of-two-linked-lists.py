606c8b58e9a9fc0524f4f0d244a8c2d7f7c6b3e0
Divyanshu Chetan <126164731+DivyanshuChetan@users.noreply.github.com>
2023-10-15 23:49:37 +0530

    Time: 146 ms (7.06%), Space: 31.7 MB (40.78%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -1,16 +1,42 @@
+#Time Complexity: O(n)
+#Space Complexity: O(n)
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        first_set=set()
-        curr=headA
-        
-        while curr:
-            first_set.add(curr)
-            curr=curr.next
-        
-        curr = headB
-        while curr:
-            if curr in first_set:
-                return curr
-            curr=curr.next
-
+        s=set()
+        while headA:
+            s.add(headA)
+            headA=headA.next
+        while headB:
+            if headB in s:
+                return headB
+            headB=headB.next
         return None
+
+#Time Complexity: O(n)
+#Space Complexity: O(1)
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        c1=c2=0
+        temp1,temp2=headA,headB
+        while temp1 or temp2:
+            if temp1:
+                c1+=1
+                temp1=temp1.next
+            if temp2:
+                c2+=1
+                temp2=temp2.next
+        c=c1-c2
+        if c<0:
+            while c!=0:
+                headB=headB.next
+                c+=1
+        else:
+            while c!=0:
+                headA=headA.next
+                c-=1
+        while headA:
+            if headA==headB:
+                return headA
+            headA=headA.next
+            headB=headB.next
+        return headA
