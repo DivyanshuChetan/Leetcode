b367943f564f107fcab6bf2eb3b1d28f0709dbdf
Divyanshu Chetan <126164731+DivyanshuChetan@users.noreply.github.com>
2023-10-16 23:58:02 +0530

    Time: 831 ms (11.61%), Space: 28.5 MB (16.5%) - LeetHub

--- 2722-prime-in-diagonal/2722-prime-in-diagonal.py
+++ 2722-prime-in-diagonal/2722-prime-in-diagonal.py
@@ -1,24 +1,21 @@
 class Solution:
-    def isPrime(self, num):
-        a = []
-        if num <= 1: return False
-        for i in range(2, isqrt(num) + 1):
-            if not num % i:
-                a.append(i)
-        return not a
-    
     def diagonalPrime(self, nums: List[List[int]]) -> int:
-        a = []
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i == j or i + j == len(nums) - 1:
-                    print(nums[i][j])
-                    a.append(nums[i][j])
-                    print(a)
-        b = []
-        for ele in a:
-            if self.isPrime(ele):
-                b.append(ele)
-        print(b)
-        return max(b) if b else 0
+        size=len(nums)
+        max=0
+        for i in range(size):
+            if self.isPrime(nums[i][i]) and max<nums[i][i]:
+                max=nums[i][i]
+            if self.isPrime(nums[i][size- i - 1]) and max<nums[i][size- i - 1]:
+                max=nums[i][size- i - 1]
+        return max
+
+    def isPrime(self,n:int) -> bool:
+        if n<2:
+            return False
+        i=2
+        while i*i<=n:
+            if n%i==0:
+                return False
+            i=i+1
+        return True
         
